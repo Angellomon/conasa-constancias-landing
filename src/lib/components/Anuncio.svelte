56cd7f6e56bcd82430375cfb1e8a233a0b875920
Angellomon <angelmtzdiaz@gmail.com>
2021-09-20 19:40:44 -0500

    feat(footer): componentes para footer

---
+++ src/lib/components/Anuncio.svelte
@@ -0,0 +1,16 @@
+<div class="anuncio">
+	"ESTE PROGRAMA ES PÚBLICO, AJENO A CUALQUIER PARTIDO POLÍTICO. QUEDA PROHIBIDO EL USO PARA FINES
+	DISTINTOS A LOS ESTABLECIDOS EN EL PROGRAMA."
+</div>
+
+<style>
+	div.anuncio {
+		font-family: 'Myriad Regular';
+		font-size: 0.9rem;
+		font-weight: bold;
+		text-align: center;
+		background-color: #cfd0d1;
+		color: rgb(22, 22, 22);
+		padding: 1.1%;
+	}
+</style>
---
+++ src/lib/components/Separator.svelte
@@ -0,0 +1,21 @@
+<span />
+
+<style>
+	span {
+		border-left: 1px solid #836019; /* Border on the left */
+		width: 0.5px; /* Width instead of height */
+		height: 50px;
+		position: relative;
+		align-self: center;
+	}
+
+	span:first-child {
+		display: none;
+	}
+
+	@media screen and (max-width: 901px) {
+		span {
+			display: none;
+		}
+	}
+</style>
