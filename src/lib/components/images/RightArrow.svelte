ac82e11676b6cd334328b13184e6d6e7f26e9aa4
Angellomon <angelmtzdiaz@gmail.com>
2021-09-22 16:42:54 -0500

    feat(images): Componentes para Loading y Flexha

---
+++ src/lib/components/images/RightArrow.svelte
@@ -0,0 +1,11 @@
+<img src="/svg/right-arrow.svg" alt="arrow" />
+
+<style>
+	img {
+		max-width: 20px;
+		filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(138deg) brightness(110%) contrast(101%);
+		position: absolute;
+		float: right;
+		margin-left: -22px;
+	}
+</style>
