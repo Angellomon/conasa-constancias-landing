f05e55341171d1cae7776e78b17bad820c5754ee
Angellomon <angelmtzdiaz@gmail.com>
2021-09-23 13:29:21 -0500

    feat(inicio): componente Boton

---
+++ src/lib/components/BotonParticipantes.svelte
@@ -0,0 +1,32 @@
+<button><a href="/participantes">PARTICIPANTES</a></button>
+
+<style>
+	button {
+		padding-left: 5px;
+		padding-right: 5px;
+		border: none;
+		border-radius: 0.4em;
+		height: 50px;
+		font-family: 'Myriad Bold';
+		color: white;
+		background-color: #836019;
+		font-size: 35px;
+		padding: 0.5rem;
+		box-shadow: -10px -10px 0px 0px #5c461a;
+		-webkit-box-shadow: -10px -10px 0px 0px #5c461a;
+		-moz-box-shadow: -10px -10px 0px 0px #5c461a;
+		cursor: pointer;
+	}
+
+	a {
+		text-decoration: none;
+		color: white;
+	}
+
+	@media screen and (min-width: 1900px) {
+		button {
+			height: 75px;
+			font-size: 45px;
+		}
+	}
+</style>
